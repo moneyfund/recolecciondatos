e515aed9cedbc3aed35d643e5a13c021bf684384
moneyfund <138159325+moneyfund@users.noreply.github.com>
2026-09-03 17:41:34 -0600

    chore: add firebase configuration template

---
+++ js/firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'REEMPLAZAR',
+  authDomain: 'REEMPLAZAR.firebaseapp.com',
+  projectId: 'REEMPLAZAR',
+  storageBucket: 'REEMPLAZAR.appspot.com',
+  messagingSenderId: 'REEMPLAZAR',
+  appId: 'REEMPLAZAR'
+};
