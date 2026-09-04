d36d642cd86899581bd2cb3443153b9a4a371cef
moneyfund <138159325+moneyfund@users.noreply.github.com>
2026-09-03 18:31:47 -0600

    Configure Firebase web app

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoOXAREVfobUyHpQcanrKYjd3j80PBifw",
+  authDomain: "recolecciondedatos-388d6.firebaseapp.com",
+  projectId: "recolecciondedatos-388d6",
+  storageBucket: "recolecciondedatos-388d6.firebasestorage.app",
+  messagingSenderId: "579530588352",
+  appId: "1:579530588352:web:6400702e49c2ab9aedad25",
+  measurementId: "G-LEGFJBPCXC"
+};
